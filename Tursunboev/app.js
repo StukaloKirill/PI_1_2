ae5e8dde93398853081b4c7e30e902aeba175fb7
fakhriddin <fakhriddin-tursunboev@mail.ru>
2018-04-11 09:02:31 +0300

    Committtt

---
+++ Tursunboev/app.js
@@ -0,0 +1,13 @@
+
+"use strict";
+
+var btn = document.getElementById("Sub");
+
+btn.onsubmit = function() {
+	alert("hello");
+}
+
+function output()
+        {
+            alert("Hello!");
+        }
--- salimov/app.js
+++
@@ -1,4 +0,0 @@
-function aClick()
-{
-  alert("Huu");
-}
